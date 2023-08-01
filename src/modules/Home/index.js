239611e6e43a20b142336d679feb23d0122bbdf6
Ranjith Gadhasu <ranjithgadhasu@gmail.com>
2023-08-01 14:20:53 +0530

    first commit

---
+++ src/modules/Home/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import Hero from '../../components/Hero'
+import Products from '../../components/Products'
+import FeatureCard from '../../components/FeatureCard'
+import Stats from '../../components/StatCard'
+import { useEffect } from 'react'
+
+const Home = () => {
+ const [products, setProducts] = useState( [] )
+   useEffect(() => {
+     const fetchProducts = async () => {
+      const response = await fetch('https://fakestoreapi.com/products?limit=12')
+      const data = await response.json()
+      console.log(data)
+      setProducts(data)
+     }
+     fetchProducts()
+  }, [])
+  
+
+  return (
+   <>
+   <Hero/>
+   <div className="flex flex-col text-center w-full mt-20">
+      <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">PRODUCTS</h2>
+      <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">MOST POPULAR PRODUCTS</h1>
+    </div>
+    {
+      products.length > 0 ?
+      <Products products={products} />
+      :
+      <div>Loading.....</div>
+    }
+  <Products/>
+  <FeatureCard/>
+  <Stats/>
+   </>
+  )
+}
+
+export default Home
